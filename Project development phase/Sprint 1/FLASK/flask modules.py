34b3d6b75e75ad145ee184feddf53f1eb2d25d9e
Reshitha-Gadhamsetty <89072062+Reshitha-Gadhamsetty@users.noreply.github.com>
2022-11-02 09:47:48 +0530

    Add files via upload

---
+++ Project development phase/Sprint 1/FLASK/flask modules.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request
+
+import os
+import cv2
+import operator
+from tensorflow.keras.models import load_model
+from werkzeug.utils import secure_filename
